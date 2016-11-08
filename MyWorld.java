f0704af106a65a016bbdb5faf8ea466a1f9c5310
Gudlauggu <gudl0025@easv365.dk>
2016-11-08 12:16:22 +0100

    just

--- MyWorld.java
+++ MyWorld.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+
 /**
  * Write a description of class MyWorld here.
  * 
@@ -17,5 +18,6 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
     }
 }
